67e544cf259a82f4d2476a273007354df8ba294d
K. Yasin <58375357+kYasinAblay@users.noreply.github.com>
2022-06-02 15:10:29 +0300

    Add project files.

---
+++ GenerationOperations/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace GenerationOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //        < Query Kind = "Statements" >
+
+            //< Reference > &lt; RuntimeDirectory & gt;\System.Drawing.dll </ Reference >
+
+            //      < Namespace > System.Drawing </ Namespace >
+            //    </ Query >
+
+            Console.WriteLine(Enumerable.Empty<int>());
+
+            Console.WriteLine(Enumerable.Repeat("hello", 3));
+
+            Console.WriteLine(Enumerable.Range(1, 10));
+            Console.WriteLine(Enumerable.Range('a', 'z' - 'a').Select(c => (char)c));
+            Console.WriteLine(Enumerable.Range(1, 10).Select(i => new string('x', i)));
+        }
+    }
+}
