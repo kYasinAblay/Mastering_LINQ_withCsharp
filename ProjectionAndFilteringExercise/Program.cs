2c12c749e6959489e8afd6111aa91b87f8aa8405
K. Yasin <58375357+kYasinAblay@users.noreply.github.com>
2022-06-03 11:04:19 +0300

    okey

---
+++ ProjectionAndFilteringExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectionAndFilteringExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static IEnumerable<int> MyFilter(IEnumerable<int> input)
+            {
+                var evenNum = input.OfType<int>().AsEnumerable();
+                var result = evenNum.Select(i => i * i);
+                result = result.Where(i => i > 50);
+
+                return result;
+            }
+
+            var numbers = Enumerable.Range(1, 10);
+
+            foreach (var list in MyFilter(numbers))
+                Console.WriteLine(list);
+
+        }
+    }
+}
