306cf3b2986b3f3dc70f0f390d0089f2846f0db4
K. Yasin <58375357+kYasinAblay@users.noreply.github.com>
2022-06-06 11:25:50 +0300

    okey

---
+++ PartitioningData_SkipWhile_TakeWhile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PartitioningData_SkipWhile_TakeWhile
+{
+    public static class ExtensionInt
+    {
+        public static void ShowInConsole(this IEnumerable<int> list)
+        {
+            foreach (var item in list)
+                Console.WriteLine(item);
+
+            Console.WriteLine("---------------------------");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //< Query Kind = "Statements" />
+
+            var numbers = new[] { 3, 3, 2, 2, 1, 1, 2, 2, 3, 3 };
+
+            numbers.Skip(2).Take(6).ShowInConsole();
+
+            new int[] { }.Take(2).ShowInConsole();
+
+            numbers.SkipWhile(i => i == 3).ShowInConsole();
+            numbers.TakeWhile(i => i > 1).ShowInConsole();
+        }
+    }
+}
---
+++ SequenceEqual_NUnitComparisons/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SequenceEqual_NUnitComparisons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //< Query Kind = "Statements" >
+
+            //  < Namespace > System.Collections </ Namespace >
+            //</ Query >
+
+            var arr1 = new[] { 1, 2, 3 };
+            var arr2 = new[] { 1, 2, 3 };
+
+            Console.WriteLine(arr1 == arr2);
+            Console.WriteLine(arr1.Equals(arr2));
+
+            // LINQ
+            Console.WriteLine(arr1.SequenceEqual(arr2));
+
+            var list1 = new List<int> { 1, 2, 3 };
+            Console.WriteLine(arr1.SequenceEqual(list1));
+
+        }
+    }
+}
